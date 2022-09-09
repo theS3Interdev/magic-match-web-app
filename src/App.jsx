03b1055947eef976f97a442f91e7066386373152
Michael Owuor <michael.owuor@gmail.com>
2022-09-09 15:51:36 +0300

    feat: setting up and shuffling cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import { useState } from 'react';
+
+const cardImages = [
+	{ src: './assets/helmet-1.png' },
+	{ src: './assets/portion-1.png' },
+	{ src: './assets/ring-1.png' },
+	{ src: './assets/scroll-1.png' },
+	{ src: './assets/sheild-1.png' },
+	{ src: './assets/sword-1.png' },
+];
 
 const App = () => {
+	const [cards, setCards] = useState([]);
+	const [turns, setTurns] = useState(0);
+
+	/** shuffle cards */
+	const shuffleCards = () => {
+		const shuffledCards = [...cardImages, ...cardImages]
+			.sort(() => Math.random() - 0.5)
+			.map((card) => ({ ...card, id: Math.random() }));
+
+		setCards(shuffledCards);
+		setTurns(0);
+	};
+
+	console.log(cards, turns);
+
 	return (
 		<div className="app">
 			<h1>Magic Match</h1>
-			<button>New Game</button>
+			<button onClick={shuffleCards}>New Game</button>
 		</div>
 	);
 };
